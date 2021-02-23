4ecf10f773e94a2c60a9f99c88d5843e55a177fe
Leeeeeeem <992969157@qq.com>
2021-02-23 11:54:04 +0800

    dev update

--- springboot1/src/main/java/com/kgc/Springboot1Application.java
+++ springboot1/src/main/java/com/kgc/Springboot1Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:/myconfig/myconfig.properties")
 public class Springboot1Application {
-
+    //dev分支修改代码
     public static void main(String[] args) {
         SpringApplication.run(Springboot1Application.class, args);
     }
